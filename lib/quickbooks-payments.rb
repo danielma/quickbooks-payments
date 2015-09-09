8d3df3de44785f9c2941539e2aebe27b998c21a6
Daniel Ma <drailskid@yahoo.com>
2015-09-08 23:32:29 -0700

    require_relative

--- lib/quickbooks-payments.rb
+++ lib/quickbooks-payments.rb
@@ -2,11 +2,11 @@ require 'oauth'
 
 module Quickbooks; end
 
-require 'quickbooks-payments/utils'
-require 'quickbooks-payments/errors'
-require 'quickbooks-payments/basic_model'
-require 'quickbooks-payments/charge'
-require 'quickbooks-payments/request'
+require_relative 'quickbooks-payments/utils'
+require_relative 'quickbooks-payments/errors'
+require_relative 'quickbooks-payments/basic_model'
+require_relative 'quickbooks-payments/charge'
+require_relative 'quickbooks-payments/request'
 
 module Quickbooks
   module Payments
